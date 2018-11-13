e4abb413bb93af83df92b44bc92b9637223aca63
Kevin D. Wolf <kevinw@software-logistics.com>
2018-11-13 18:08:01 -0500

    Add basic OpenFAAS example

---
+++ OpenFAAS/pem-handler/pem-handler/FunctionHandler.cs
@@ -0,0 +1,37 @@
+﻿using LagoVista.Core;
+using LagoVista.Core.Validation;
+using LagoVista.IoT.Runtime.Core.Models.PEM;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public async Task<string> Handle(string json)
+        {
+            try
+            {
+                var pem = JsonConvert.DeserializeObject<LagoVista.IoT.Runtime.Core.Models.PEM.PipelineExecutionMessage>(json);
+
+                pem.Log.Add(new LagoVista.IoT.Logging.Info()
+                {
+                    Message = "I have been processed by an external script.",
+                    TimeStamp = DateTime.Now.ToJSONString()
+                });
+
+                await Task.Delay(500);
+
+                return JsonConvert.SerializeObject(InvokeResult<PipelineExecutionMessage>.Create(pem),
+                    new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+            }
+            catch(Exception ex)
+            {
+                return  JsonConvert.SerializeObject(InvokeResult<PipelineExecutionMessage>.FromError(ex.Message),
+                    new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+            }
+        }
+    }
+}
