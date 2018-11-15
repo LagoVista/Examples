a1133736a5874eaf461b40f81aa4af5449ff2d75
Kevin D. Wolf <kevinw@software-logistics.com>
2018-11-15 13:49:30 -0500

    updates to cyclone power

--- WindTurbineClient/WindTurbineClient/Program.cs
+++ WindTurbineClient/WindTurbineClient/Program.cs
@@ -1,18 +1,21 @@
 ﻿using OpenNETCF.MQTT;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WindTurbineClient
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var client = new OpenNETCF.MQTT.MQTTClient("mqttdev.nuviot.com", 1883);
-            client.Connect("statreader", "kevinw", "XXXX");
+
+            var client = new OpenNETCF.MQTT.MQTTClient("cyclone-mqtt.iothost.net", 1883);
+            client.Connect("statreader", "cyclone", "XXXXXXX");
             client.Connected += Client_Connected;
             client.MessageReceived += Client_MessageReceived;
             Console.WriteLine("CTRL+C = ENd");
@@ -21,9 +24,22 @@ namespace WindTurbineClient
 
         private static void Client_MessageReceived(string topic, QoS qos, byte[] payload)
         {
-            var msg = System.Text.ASCIIEncoding.ASCII.GetString(payload);
-            Console.WriteLine(topic);
-            Console.WriteLine(msg);
+            if (topic.StartsWith("turbine/status"))
+            {
+                var msg = System.Text.ASCIIEncoding.ASCII.GetString(payload);
+                var obj = JsonConvert.DeserializeObject<StatusMessage>(msg);
+                Console.WriteLine($"Turbine Id: {obj.TurbineId}, RPM: {obj.RPM}, Power: {obj.Power}");
+            }
+            else if(topic.StartsWith("turbine/power"))
+            {
+                var parts = topic.Split('/');
+                if (parts.Length == 4)
+                {
+                    var turbineId = parts[2];
+                    var powerStatus = parts[3];
+                    Console.WriteLine($"Turbine Id: {turbineId}, Status: {powerStatus}");
+                }
+            }
         }
 
         private static void Client_Connected(object sender, EventArgs e)
@@ -32,5 +48,19 @@ namespace WindTurbineClient
             client.Subscriptions.Add("#", QoS.FireAndForget);
             
         }
+    }
+
+    class StatusMessage
+    {
+        [JsonProperty("turbineId")]
+        public String TurbineId { get; set; }
+        [JsonProperty("rpm")]
+        public double RPM { get; set; }
+        [JsonProperty("power")]
+        public double Power { get; set; }
+        [JsonProperty("timeStamp")]
+        public string TimeStamp { get; set; }
+
+
     }
 }
